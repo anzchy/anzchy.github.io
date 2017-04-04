59e21c4bf6f337e58addb76e635917e565c9d71d
anzchy <anzchy@163.com>
2017-04-04 23:41:29 +0800

    Site updated: 2017-04-04 23:41:28

---
+++ js/src/custom/custom.js
@@ -0,0 +1,6 @@
+/* custom reading bar */
+$(document).ready(function () {
+  $(window).scroll(function(){
+    $(".top-scroll-bar").attr("style", "width: " + ($(this).scrollTop() / ($(document).height() - $(this).height()) * 100) + "%; display: block;");
+  });
+});
